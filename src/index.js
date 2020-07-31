eddb25e92a520bac7e9e3084e1db55c15e63e4f0
Koki Oyatsu <kaishuu0123@gmail.com>
2020-07-31 18:18:50 +0900

    Follow markdown-it fence.js
    * From
    https://github.com/markdown-it/markdown-it/blob/master/lib/rules_block/fence.js

--- src/index.js
+++ src/index.js
@@ -1,88 +1,124 @@
-'use strict'
+'use strict';
 
-export default function (md, name, opts) {
-  function defaultValidate(params) {
+var index = function (md, name, options) {
+  function validateDefault(params) {
     return params.trim().split(' ', 2)[0] === name
   }
 
-  function defaultRender(tokens, idx, _options, env, self) {
+  function renderDefault(tokens, idx, _options, env, self) {
     if (tokens[idx].nesting === 1) {
-      tokens[idx].attrPush(['class', name])
+      tokens[idx].attrPush(['class', name]);
     }
 
     return self.renderToken(tokens, idx, _options, env, self)
   }
 
-  const options = Object.assign({
-    validate: defaultValidate,
-    render: defaultRender
-  }, opts)
+  options = options || {}
 
-  function fence(state, startLine, endLine) {
-    const optionMarker = options.marker || '`'
-    let pos = state.bMarks[startLine] + state.tShift[startLine]
-    let max = state.eMarks[startLine]
-    let haveEndMarker = false
+  var min_markers = 3,
+      marker_str  = options.marker || '`',
+      marker_char = marker_str.charCodeAt(0),
+      marker_len  = marker_str.length,
+      validate    = options.validate || validateDefault,
+      render      = options.render || renderDefault;
 
-    if (state.sCount[startLine] - state.blkIndent >= 4) return false
-    if (pos + 3 > max) return false
+  function fence(state, startLine, endLine, silent) {
+    var marker, len, params, nextLine, mem, token, markup,
+      haveEndMarker = false,
+      pos = state.bMarks[startLine] + state.tShift[startLine],
+      max = state.eMarks[startLine];
 
-    const marker = state.src.charCodeAt(pos)
+    // if it's indented more than 3 spaces, it should be a code block
+    if (state.sCount[startLine] - state.blkIndent >= 4) { return false; }
 
-    if (marker !== optionMarker.charCodeAt(0)) return false
+    if (pos + 3 > max) { return false; }
 
-    let mem = pos
-    pos = state.skipChars(pos, marker)
-    let len = pos - mem
+    marker = state.src.charCodeAt(pos);
 
-    if (len < 3) return false
-
-    const markup = state.src.slice(mem, pos)
-    const params = state.src.slice(pos, max)
-
-    if (params.indexOf(String.fromCharCode(marker)) >= 0) return false
-    if (!options.validate(params)) { return false }
-
-    let nextLine = startLine
-
-    for (;;) {
-      nextLine++
-      if (nextLine >= endLine) break
-
-      pos = mem = state.bMarks[nextLine] + state.tShift[nextLine]
-      max = state.eMarks[nextLine]
-
-      if (pos < max && state.sCount[nextLine] < state.blkIndent) break
-      if (state.src.charCodeAt(pos) !== marker) continue
-      if (state.sCount[nextLine] - state.blkIndent >= 4) continue
-
-      pos = state.skipChars(pos, marker)
-
-      if (pos - mem < len) continue
-
-      pos = state.skipSpaces(pos)
-
-      if (pos < max) continue
-
-      haveEndMarker = true
-
-      break
+    if (marker !== marker_char) {
+      return false;
     }
 
-    len = state.sCount[startLine]
-    state.line = nextLine + (haveEndMarker ? 1 : 0)
+    // scan marker length
+    mem = pos;
+    pos = state.skipChars(pos, marker);
 
-    let token
-    token = state.push(name, 'div', 0)
-    token.info = params
-    token.content = state.getLines(startLine + 1, nextLine, len, true)
-    token.markup = markup
-    token.map = [startLine, state.line]
+    len = pos - mem;
+
+    if (len < 3) { return false; }
+
+    markup = state.src.slice(mem, pos);
+    params = state.src.slice(pos, max);
+
+    if (marker === marker_char) {
+      if (params.indexOf(String.fromCharCode(marker)) >= 0) {
+        return false;
+      }
+    }
+
+    // Since start is found, we can report success here in validation mode
+    if (silent) { return true; }
+
+    // search end of block
+    nextLine = startLine;
+
+    for (;;) {
+      nextLine++;
+      if (nextLine >= endLine) {
+        // unclosed block should be autoclosed by end of document.
+        // also block seems to be autoclosed by end of parent
+        break;
+      }
+
+      pos = mem = state.bMarks[nextLine] + state.tShift[nextLine];
+      max = state.eMarks[nextLine];
+
+      if (pos < max && state.sCount[nextLine] < state.blkIndent) {
+        // non-empty line with negative indent should stop the list:
+        // - ```
+        //  test
+        break;
+      }
+
+      if (state.src.charCodeAt(pos) !== marker_char) { continue; }
+
+      if (state.sCount[nextLine] - state.blkIndent >= 4) {
+        // closing fence should be indented less than 4 spaces
+        continue;
+      }
+
+      pos = state.skipChars(pos, marker);
+
+      // closing code fence must be at least as long as the opening one
+      if (pos - mem < len) { continue; }
+
+      // make sure tail has spaces only
+      pos = state.skipSpaces(pos);
+
+      if (pos < max) { continue; }
+
+      haveEndMarker = true;
+      // found!
+      break;
+    }
+
+    // If a fence has heading spaces, they should be removed from its inner block
+    len = state.sCount[startLine]
+
+    state.line = nextLine + (haveEndMarker ? 1 : 0);
+
+    token = state.push(name, 'div', 0);
+    token.info = params;
+    token.content = state.getLines(startLine + 1, nextLine, len, true);
+    token.markup = markup;
+    token.map = [startLine + 1, state.line];
 
     return true
   }
 
   md.block.ruler.before('fence', name, fence, {
-    alt: ['paragraph', 'reference', 'blockquote']})
-  md.renderer.rules[name] = options.render
-}
+    alt: ['paragraph', 'reference', 'blockquote']});
+  md.renderer.rules[name] = options.render;
+};
+
+module.exports = index;
